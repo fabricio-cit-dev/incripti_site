6c90df2600068da86a9952ff6d5ed6232aab4555
fabricio-cit-dev <fabricio.feo@citechnology.com.br>
2023-05-22 18:57:24 -0300

    banner, fale conosco, logo

---
+++ src/components/Contact/ContactOneForm.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const ContactOneForm = () => {
+  return (
+    <div className="contact-form-wrap">
+      <form action="#">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="form-grp">
+              <input type="text" placeholder="Nome Completo" required />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="form-grp">
+              <input type="email" placeholder="Email" required />
+            </div>
+          </div>
+        </div>
+        <div className="form-grp">
+          <textarea name="massage" placeholder="Nos fale sobre seu projeto!"></textarea>
+        </div>
+        <div className="submit-btn text-center">
+          <button type="submit" className="btn">
+            Enviar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ContactOneForm;
